efa89df32d4a723db0af138ddd37a280d990fd68
p-rahimi <parisa_rahimy@yahoo.com>
2025-02-08 12:06:25 +0200

    first commit

---
+++ db/forum.sql
@@ -0,0 +1,145 @@
+-- SQLite
+DROP TABLE IF EXISTS "comment_likes";
+DROP TABLE IF EXISTS "post_likes";
+DROP TABLE IF EXISTS "post_files";
+DROP TABLE IF EXISTS "comments";
+DROP TABLE IF EXISTS "post_categories";
+DROP TABLE IF EXISTS "posts";
+DROP TABLE IF EXISTS "categories";
+DROP TABLE IF EXISTS  "sessions";
+DROP TABLE IF EXISTS "users";
+
+CREATE TABLE "categories" (
+  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+  "name" TEXT NOT NULL,
+  "status" TEXT NOT NULL CHECK ("status" IN ('enable', 'disable', 'delete')) DEFAULT 'enable',
+  "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "created_by" INTEGER NOT NULL,
+  "updated_at" DATETIME,
+  "updated_by" INTEGER,
+  FOREIGN KEY (created_by) REFERENCES "users" ("id"),
+  FOREIGN KEY (updated_by) REFERENCES "users" ("id")
+);
+CREATE TABLE "users" (
+  "id" INTEGER PRIMARY KEY,
+  "uuid" TEXT NOT NULL UNIQUE,
+  "type" TEXT NOT NULL CHECK ("type" IN ('admin', 'normal_user', 'test_user')) DEFAULT 'normal_user',
+  "name" TEXT,
+  "username" TEXT UNIQUE,
+  "email" TEXT UNIQUE,
+  "profile_photo" TEXT NULL,
+  "password" TEXT,
+  "status" TEXT NOT NULL CHECK ("status" IN ('enable', 'disable', 'delete')) DEFAULT 'enable',
+  "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "updated_at" DATETIME,
+  "updated_by" INTEGER,
+  FOREIGN KEY (updated_by) REFERENCES "users" ("id")
+);
+CREATE TABLE "posts" (
+  "id" INTEGER PRIMARY KEY,
+  "uuid" TEXT NOT NULL UNIQUE,
+  "title" TEXT NOT NULL,
+  "description" TEXT NOT NULL,
+  "status" TEXT NOT NULL CHECK ("status" IN ('enable', 'disable', 'delete')) DEFAULT 'enable',
+  "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "user_id" INTEGER NOT NULL,
+  "updated_at" DATETIME,
+  "updated_by" INTEGER,
+  FOREIGN KEY (user_id) REFERENCES "users" ("id"),
+  FOREIGN KEY (updated_by) REFERENCES "users" ("id")
+);
+
+CREATE TABLE "post_files" (
+  "id" INTEGER PRIMARY KEY,
+  "post_id" INTEGER NOT NULL,
+  "file_uploaded_name" TEXT NOT NULL,
+  "file_real_name" TEXT NOT NULL,
+  "status" TEXT NOT NULL CHECK ("status" IN ('enable', 'delete')) DEFAULT 'enable',
+  "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "created_by" INTEGER NOT NULL,
+  "updated_at" DATETIME,
+  "updated_by" INTEGER,
+  FOREIGN KEY (post_id) REFERENCES "posts" ("id"),
+  FOREIGN KEY (updated_by) REFERENCES "users" ("id")
+);
+
+CREATE TABLE "post_likes" (
+  "id" INTEGER PRIMARY KEY,
+  "type" TEXT NOT NULL CHECK ("type" IN ('like', 'dislike')),
+  "post_id" INTEGER NOT NULL,
+  "user_id" INTEGER NOT NULL,
+  "status" TEXT NOT NULL CHECK ("status" IN ('enable', 'delete')) DEFAULT 'enable',
+  "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "updated_at" DATETIME,
+  "updated_by" INTEGER,
+  FOREIGN KEY (user_id) REFERENCES "users" ("id"),
+  FOREIGN KEY (post_id) REFERENCES "posts" ("id"),
+  FOREIGN KEY (updated_by) REFERENCES "users" ("id")
+);
+
+CREATE TABLE "post_categories" (
+  "id" INTEGER PRIMARY KEY,
+  "post_id" INTEGER NOT NULL,
+  "category_id" INTEGER NOT NULL,
+  "status" TEXT NOT NULL CHECK ("status" IN ('enable', 'disable', 'delete')) DEFAULT 'enable',
+  "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "created_by" INTEGER NOT NULL,
+  "updated_at" DATETIME,
+  "updated_by" INTEGER,
+  FOREIGN KEY (created_by) REFERENCES "users" ("id"),
+  FOREIGN KEY (updated_by) REFERENCES "users" ("id"),
+  FOREIGN KEY (post_id) REFERENCES "posts" ("id"),
+  FOREIGN KEY (category_id) REFERENCES "categories" ("id")
+);
+
+CREATE TABLE "comments" (
+  "id" INTEGER PRIMARY KEY,
+  "post_id" INTEGER NOT NULL,
+  "description" TEXT NOT NULL,
+  "user_id" INTEGER NOT NULL,
+  "status" TEXT NOT NULL CHECK ("status" IN ('enable', 'disable', 'delete')) DEFAULT 'enable',
+  "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "updated_at" DATETIME,
+  "updated_by" INTEGER,
+  FOREIGN KEY (user_id) REFERENCES "users" ("id"),
+  FOREIGN KEY (updated_by) REFERENCES "users" ("id"),
+  FOREIGN KEY (post_id) REFERENCES "posts" ("id")
+);
+
+CREATE TABLE "comment_likes" (
+  "id" INTEGER PRIMARY KEY,
+  "type" TEXT NOT NULL,
+  "comment_id" INTEGER NOT NULL,
+  "user_id" INTEGER NOT NULL,
+  "status" TEXT NOT NULL CHECK ("status" IN ('enable', 'delete')) DEFAULT 'enable',
+  "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  "updated_at" DATETIME,
+  "updated_by" INTEGER,
+  FOREIGN KEY (user_id) REFERENCES "users" ("id"),
+  FOREIGN KEY (updated_by) REFERENCES "users" ("id"),
+  FOREIGN KEY (comment_id) REFERENCES "comments" ("id")
+);
+
+CREATE TABLE "sessions" (
+  "id" INTEGER PRIMARY KEY,
+  "session_token" TEXT NOT NULL UNIQUE,
+  "user_id" INTEGER NOT NULL,
+  "expires_at" DATETIME NOT NULL,
+  "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (user_id) REFERENCES "users" ("id")
+);
+
+INSERT INTO users(uuid, type,name,username,password, email)
+VALUES ('67921bdd-8458-800e-b9d4-065a43242cd3', 'admin', 'admin', 'admin', '$2a$10$DN.v/NkfQjmPaTTz15x0E.u8l2R9.HnB12DpDVMdRPeQZDfMwovSa', 'admin@admin');
+
+INSERT INTO categories (name, created_by)
+VALUES ('art', 1), ('science', 1), ('news', 1);
+
+INSERT INTO posts(uuid, title, description, user_id)
+VALUES ('f9edb8d6-c739-4d6f-aaa4-9b298f2e1552', 'first post', 'this is first post of forum that is made by admin', 1);
+
+INSERT INTO post_categories(post_id, category_id, created_by)
+VALUES (1, 1, 1), (1, 2, 1);
+
+INSERT INTO comments(post_id, description, user_id)
+VALUES (1, 'this is first post comment that is made by admin', 1);
---
+++ modules/userManagement/models/user.go
@@ -0,0 +1,121 @@
+package models
+
+import (
+	"database/sql"
+	"errors"
+	"forum/db"
+	"forum/utils"
+	"log"
+	"time"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+// User struct represents the user data model
+type User struct {
+	ID           int        `json:"id"`
+	UUID         string     `json:"uuid"`
+	Type         string     `json:"type"`
+	Name         string     `json:"name"`
+	Username     string     `json:"username"`
+	Email        string     `json:"email"`
+	Password     string     `json:"password"`
+	ProfilePhoto string     `json:"profile_photo"`
+	Status       string     `json:"status"`
+	CreatedAt    time.Time  `json:"created_at"`
+	UpdatedAt    *time.Time `json:"updated_at"`
+	UpdatedBy    *int       `json:"updated_by"`
+}
+
+func InsertUser(user *User) (int, error) {
+	db := db.OpenDBConnection()
+	defer db.Close() // Close the connection after the function finishes
+
+	// Generate UUID for the user if not already set
+	if user.UUID == "" {
+		uuid, err := utils.GenerateUuid()
+		if err != nil {
+			return -1, err
+		}
+		user.UUID = uuid
+	}
+
+	var existingEmail string
+	var existingUsername string
+	emailCheckQuery := `SELECT email, username FROM users WHERE email = ? OR username = ? LIMIT 1;`
+	err := db.QueryRow(emailCheckQuery, user.Email, user.Username).Scan(&existingEmail, &existingUsername)
+	if err == nil {
+		if existingEmail == user.Email {
+			return -1, errors.New("duplicateEmail")
+		}
+		if existingUsername == user.Username {
+			return -1, errors.New("duplicateUsername")
+		}
+	}
+
+	insertQuery := `INSERT INTO users (uuid, name, username, email, password) VALUES (?, ?, ?, ?, ?);`
+	result, insertErr := db.Exec(insertQuery, user.UUID, user.Username, user.Username, user.Email, user.Password)
+	if insertErr != nil {
+		// Check if the error is a SQLite constraint violation (duplicate entry)
+		if sqliteErr, ok := insertErr.(interface{ ErrorCode() int }); ok {
+			if sqliteErr.ErrorCode() == 19 { // 19 = UNIQUE constraint failed (SQLite error code)
+				return -1, errors.New("user with this email or username already exists")
+			}
+		}
+		return -1, insertErr // Other DB errors
+	}
+
+	// Retrieve the last inserted ID
+	userId, err := result.LastInsertId()
+	if err != nil {
+		log.Fatal(err)
+		return -1, err
+	}
+
+	return int(userId), nil
+}
+
+func UpdateProfilePhoto(user *User) error {
+	db := db.OpenDBConnection()
+	defer db.Close() // Close the connection after the function finishes
+
+	updateUser := `UPDATE users
+					SET profile_photo = ?,
+						updated_at = CURRENT_TIMESTAMP,
+						updated_by = ?
+					WHERE id = ?;`
+	_, updateErr := db.Exec(updateUser, user.ProfilePhoto, user.ID, user.ID)
+	if updateErr != nil {
+		return updateErr
+	}
+
+	return nil
+}
+func AuthenticateUser(username, password string) (bool, int, error) {
+	// Open SQLite database
+	db := db.OpenDBConnection()
+	defer db.Close() // Close the connection after the function finishes
+
+	// Query to retrieve the hashed password stored in the database for the given username
+	var userId int
+	var storedHashedPassword string
+	err := db.QueryRow("SELECT id, password FROM users WHERE username = ?", username).Scan(&userId, &storedHashedPassword)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			// Username not found
+			return false, -1, errors.New("username not found")
+		}
+		// Handle other database errors
+		log.Fatal(err)
+	}
+
+	// Compare the entered password with the stored hashed password using bcrypt
+	err = bcrypt.CompareHashAndPassword([]byte(storedHashedPassword), []byte(password))
+	if err != nil {
+		// Password is incorrect
+		return false, -1, errors.New("password is incorrect")
+	}
+
+	// Successful login if no errors occurred
+	return true, userId, nil
+}
